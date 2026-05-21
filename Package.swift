efdb73c37a5c06968b6c2ba97bc89eede4bbad99
Amir <amirmoradabadi@gmail.com>
2026-05-21 16:24:17 +0200

    chore: bump DropOnAir iOS SDK to 0.13.0

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     name: "DropOnAirDemo",
     platforms: [.iOS(.v16), .macOS(.v13)],
     dependencies: [
-        .package(url: "https://github.com/DropOnAir/droponair-sdk-ios-binary.git", from: "0.12.0"),
+        .package(url: "https://github.com/DropOnAir/droponair-sdk-ios-binary.git", from: "0.13.0"),
     ],
     targets: [
         .executableTarget(
